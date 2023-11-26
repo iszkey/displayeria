c1c2109f76fb047d0a6e32ba5b2981818ed8b70c
iszkey <kisozaki1026@gmail.com>
2023-11-26 21:46:15 +0900

    キャラクターデータの入力・解析機能の追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 
+import { Character } from "@/types/character";
 import { useState } from "react";
 
+// キャラクター情報を入力して、その情報を表示するコンポーネント
 export default function Home() {
-  const [character, setCharacter] = useState("");
+  const [character, setCharacter] = useState<Character | null>(null);
+
+  // 入力フォームの変更を処理する関数
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue: string = e.target.value;
+    try {
+      const characterData: Character = JSON.parse(inputValue);
+      setCharacter(characterData);
+    } catch (error) {
+      alert("入力データのフォーマットが不正です");
+    }
+  };
 
   return (
     <div>
-      <input
-        value={character}
-        onChange={(e) => setCharacter(e.target.value)}
-        className="text-black"
-      />
+      <input onChange={handleInputChange} className="text-black" />
     </div>
   );
 }
