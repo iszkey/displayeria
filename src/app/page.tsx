a10c0df677127049c8d5963b8ead2f8d1db8a6a8
iszkey <kisozaki1026@gmail.com>
2023-11-26 21:58:16 +0900

    ペーストイベントを取得する方式に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,34 @@
 "use client";
 
 import { Character } from "@/types/character";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // キャラクター情報を入力して、その情報を表示するコンポーネント
 export default function Home() {
   const [character, setCharacter] = useState<Character | null>(null);
 
-  // 入力フォームの変更を処理する関数
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue: string = e.target.value;
-    try {
-      const characterData: Character = JSON.parse(inputValue);
-      setCharacter(characterData);
-    } catch (error) {
-      alert("入力データのフォーマットが不正です");
-    }
-  };
+  useEffect(() => {
+    // ペーストされたデータを処理するハンドラ
+    const handlePaste = (e: ClipboardEvent) => {
+      const text = e.clipboardData?.getData("text");
+      if (text) {
+        try {
+          const characterData: Character = JSON.parse(text);
+          setCharacter(characterData);
+        } catch (error) {
+          alert("入力データのフォーマットが不正です");
+        }
+      }
+    };
 
-  return (
-    <div>
-      <input onChange={handleInputChange} className="text-black" />
-    </div>
-  );
+    // ページ全体でのペーストイベントをリスン
+    document.addEventListener("paste", handlePaste);
+
+    // クリーンアップ関数
+    return () => {
+      document.removeEventListener("paste", handlePaste);
+    };
+  }, []);
+
+  return <div>好きなところにペーストしてね</div>;
 }
